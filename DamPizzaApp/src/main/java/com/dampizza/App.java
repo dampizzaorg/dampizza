17f9c716480897ddecaf1e7953a1cfc7cc543b2a
2dam <2dam@MACINF09.TartangaLH.eus>
2018-02-21 10:44:37 +0100

    Vistas en español

--- DamPizzaApp/src/main/java/com/dampizza/App.java
+++ DamPizzaApp/src/main/java/com/dampizza/App.java
@@ -114,7 +114,7 @@ public class App extends MobileApplication {
 
         addLayerFactory(MENU_LAYER, () -> new SidePopupView(new DrawerManager().getDrawer()));
 
-        //testHibernate();
+        testHibernate();
     }
 
     @Override
